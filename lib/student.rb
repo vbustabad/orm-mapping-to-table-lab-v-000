eeb3ff798f5d8155573ec93e7109c5a5af21ce8a
Veronica Bustabad <vbustabad@gmail.com>
2018-06-25 03:10:48 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -43,7 +43,7 @@ class Student
   end
 
   def self.create(name:, grade:)
-    new_student = Student.new(name:, grade:).save
+    new_student = Student.new(name, grade).save
     new_student
   end
 
