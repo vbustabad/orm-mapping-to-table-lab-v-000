ac7d96d8ebaf55f03bf5ef5fbd0efbf6bebee226
Veronica Bustabad <vbustabad@gmail.com>
2018-06-25 02:44:07 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -41,9 +41,9 @@ class Student
 
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
-  
+
   def self.create(name:, grade:)
     Student.new(name, grade)
-  end 
+  end
 
 end
